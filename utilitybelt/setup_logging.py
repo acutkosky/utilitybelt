504e9cccb1597295fd6a6579120a59c01833b932
Ashok Cutkosky <ashok@cutkosky.com>
2024-10-23 17:31:16 -0400

    add ability to overwrite other packages config

--- utilitybelt/setup_logging.py
+++ utilitybelt/setup_logging.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def setup(log_level="INFO"):
+def setup(log_level="INFO", force_reconfigure=False):
     level = getattr(logging, log_level.upper())
     
     # logging.basicConfig(format='%(levelname)s:%(message)s', level=level)
@@ -9,7 +9,7 @@ def setup(log_level="INFO"):
     debug_handler.setLevel(logging.DEBUG)
 
     # Define the log format for DEBUG messages (logger name, filename, line number, and message)
-    debug_formatter = logging.Formatter('%(levelname)s:%(name)s:%(filename)s:%(lineno)d: %(message)s')
+    debug_formatter = logging.Formatter("[%(asctime)s][%(levelname)s][%(name)s][%(filename)s:%(lineno)d] - %(message)s")
     debug_handler.setFormatter(debug_formatter)
 
     # Add a filter so that the DEBUG messages are handled only by this handler
@@ -18,13 +18,26 @@ def setup(log_level="INFO"):
 
     non_debug_handler = logging.StreamHandler()
     non_debug_handler.setLevel(level)  # Set to the passed log level (default INFO)
-    non_debug_formatter = logging.Formatter('%(levelname)s:%(name)s: %(message)s')
+    non_debug_formatter = logging.Formatter("[%(asctime)s][%(levelname)s][%(name)s] - %(message)s")
     non_debug_handler.setFormatter(non_debug_formatter)
     non_debug_handler.addFilter(lambda record: record.levelno >= level and record.levelno != logging.DEBUG)
 
 
-    logging.basicConfig(
-        format='%(levelname)s:%(name)s: %(message)s',
-        level=logging.DEBUG,
-        handlers=[debug_handler, non_debug_handler]
-    )
+    if not force_reconfigure:
+        # logging.basicConfig will not do anything if
+        # someone else has already used logging.
+        logging.basicConfig(
+            level=level,
+            handlers=[debug_handler, non_debug_handler]
+        )
+    else:
+        root_logger = logging.getLogger()
+        
+        # nuke any other configurations
+        while root_logger.hasHandlers():
+            root_logger.removeHandler(root_logger.handlers[0])
+
+        root_logger.addHandler(debug_handler)
+        root_logger.addHandler(non_debug_handler)
+        root_logger.setLevel(level)
+            
